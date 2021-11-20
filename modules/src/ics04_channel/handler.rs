8da1b06a011303eb61653a3e2fa2a590e551a107
Boern <boern.x@gmail.com>
2021-11-20 19:42:01 +0800

    update write ack

--- modules/src/ics04_channel/handler.rs
+++ modules/src/ics04_channel/handler.rs
@@ -8,6 +8,7 @@ use crate::ics04_channel::msgs::ChannelMsg;
 use crate::ics04_channel::{msgs::PacketMsg, packet::PacketResult};
 use crate::ics05_port::capabilities::Capability;
 use crate::ics24_host::identifier::{ChannelId, PortId};
+use crate::ics26_routing::context::Ics26Context;
 use crate::prelude::*;
 
 pub mod acknowledgement;
@@ -64,27 +65,41 @@ where
 }
 
 /// Dispatcher for processing any type of message related to the ICS4 packet protocols.
-pub fn packet_dispatch<Ctx>(ctx: &Ctx, msg: PacketMsg) -> Result<HandlerOutput<PacketResult>, Error>
+pub fn packet_dispatch<Ctx>(
+    ctx: &mut Ctx,
+    msg: PacketMsg,
+) -> Result<HandlerOutput<PacketResult>, Error>
 where
-    Ctx: ChannelReader,
+    Ctx: Ics26Context,
 {
     match msg {
-        PacketMsg::RecvPacket(msg) => recv_packet::process(ctx, msg),
+        PacketMsg::RecvPacket(msg) => {
+            let recv_handler_out = recv_packet::process(ctx, msg.clone())?;
+            // store_packet_result not really unimploment!
+            ctx.store_packet_result(recv_handler_out.result)?;
+
+            // TODO: callback and return ack
+            // let act = ctx.OnRecvPacket(msg.packet);
+            // handle write ack
+            let ack = vec![1]; // a mock ack value
+            let packet = msg.packet;
+            let write_ack_handler_out = write_acknowledgement::process(ctx, packet, ack)?;
+
+            // 把recv packet和write ack两个事件及log合并到一起返回，但result只能返回一个，因此会丢掉另外一个result，M1阶段暂时返回write ack的result
+            // 有两个方案可以考虑把多个result同时返回：
+            // 方案1：修改HandlerOut中的Result类型，改为Vec<HandlerResult>，容纳多个result，但这样以来所有用到handlerout的地方都想需要重构！
+            // 方案2：想办法再调用一次deliever(),然后匹配write_acknowledgement消息，做一次单独处理，改动也不小！
+            // 可能还存在其他解决方案，但是需要结合需求以及目前的架构综合考虑。
+            // 幸好substrate pallet 那边只需要保存height数据，其他暂时用不到，而这个height可以从event中获取！
+            Ok(HandlerOutput::builder()
+                .with_log(recv_handler_out.log)
+                .with_events(recv_handler_out.events)
+                .with_log(write_ack_handler_out.log)
+                .with_events(write_ack_handler_out.events)
+                .with_result(write_ack_handler_out.result))
+        }
         PacketMsg::AckPacket(msg) => acknowledgement::process(ctx, msg),
         PacketMsg::ToPacket(msg) => timeout::process(ctx, msg),
         PacketMsg::ToClosePacket(msg) => timeout_on_close::process(ctx, msg),
-    }
-}
-
-// Todo: Apply proper errors
-pub fn write_ack_packet_dispatch<Ctx>(ctx: &Ctx, msg: PacketMsg, ack: Vec<u8>) -> HandlerResult<PacketResult, Error>
-    where
-        Ctx: ChannelReader,
-{
-    match msg {
-        PacketMsg::RecvPacket(msg) => write_acknowledgement::process(ctx, msg.packet, ack),
-        PacketMsg::AckPacket(msg) => Err(Error::invalid_acknowledgement()),  // Todo:
-        PacketMsg::ToPacket(msg) => Err(Error::invalid_acknowledgement()),  // Todo:
-        PacketMsg::ToClosePacket(msg) => Err(Error::invalid_acknowledgement()),  // Todo:
     }
 }
