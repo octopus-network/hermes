0e7b847acdb6e21ed5e80882e7d9b0e5b8d829e0
livelybug <livelybug@gmail.com>
2022-02-22 18:14:00 +0800

    Use retry_with_index in method proven_packet

--- relayer/src/chain/substrate.rs
+++ relayer/src/chain/substrate.rs
@@ -1680,46 +1680,44 @@ impl ChainEndpoint for SubstrateChain {
         tracing::info!("in Substrate: [proven_packet] >> sequence = {:?}", sequence);
         tracing::info!("in Substrate: [proven_packet] >> height = {:?}", height);
 
-        let packet_result = async {
-            let client = ClientBuilder::new()
-                .set_url(&self.websocket_url.clone())
-                .build::<ibc_node::DefaultConfig>()
-                .await
-                .unwrap();
+        let result = retry_with_index(Fixed::from_millis(200), |current_try| {
+            if current_try > MAX_QUERY_TIMES {
+                return RetryResult::Err("did not succeed within tries");
+            }
 
-            sleep(Duration::from_secs(4)).await;
+            let result = async {
+                let client = ClientBuilder::new()
+                    .set_url(&self.websocket_url.clone())
+                    .build::<ibc_node::DefaultConfig>()
+                    .await
+                    .unwrap();
 
-            let packet_result:Vec<u8> = match packet_type {
-                PacketMsgType::Recv => {
-                    let data = octopusxt::call_ibc::get_packet_commitment(&port_id, &channel_id, sequence.0, client).await.unwrap();
-                    tracing::info!(
-                        "In Substrate: [proven_packet] >> packet_commitment: {:?}", String::from_utf8(data.clone()).unwrap()
-                    );
-                    data
+                match packet_type {
+                    PacketMsgType::Recv => {
+                        octopusxt::call_ibc::get_packet_commitment(&port_id, &channel_id, sequence.0, client).await
+                    }
+                    PacketMsgType::Ack => {
+                        octopusxt::call_ibc::get_packet_ack(&port_id, &channel_id, sequence.0, client).await
+                    }
+                    _ => unimplemented!(),
                 }
-                PacketMsgType::Ack => {
-                    let data = octopusxt::call_ibc::get_packet_ack(&port_id, &channel_id, sequence.0, client).await.unwrap();
-                    tracing::info!(
-                        "In Substrate: [proven_packet] >> packet_ack: {:?}", String::from_utf8(data.clone()).unwrap()
-                    );
-                    data
-                }
-                _ => unimplemented!(),
             };
 
-            packet_result
-        };
-
-        let packet_result = self.block_on(packet_result);
+            match self.block_on(result) {
+                Ok(v) => RetryResult::Ok(v),
+                Err(e) => RetryResult::Retry("Fail to retry"),
+            }
+        });
+        tracing::info!("In Substrate: [proven_packet] >> result: {:?}, packet_type = {:?}", result, packet_type);
 
         match packet_type {
             PacketMsgType::Recv => {
                 let storage_entry = ibc_node::ibc::storage::PacketCommitment(port_id.as_bytes().to_vec(), channel_id.as_bytes().to_vec(), u64::from(sequence.clone()).encode());
-                Ok((packet_result, self.generate_storage_proof(&storage_entry, &height, "PacketCommitment")))
+                Ok((result.unwrap(), self.generate_storage_proof(&storage_entry, &height, "PacketCommitment")))
             }
             PacketMsgType::Ack => {
                 let storage_entry = ibc_node::ibc::storage::Acknowledgements(port_id.as_bytes().to_vec(), channel_id.as_bytes().to_vec(), u64::from(sequence.clone()).encode());
-                Ok((packet_result, self.generate_storage_proof(&storage_entry, &height, "Acknowledgements")))
+                Ok((result.unwrap(), self.generate_storage_proof(&storage_entry, &height, "Acknowledgements")))
             }
             _ => unimplemented!(),
         }
