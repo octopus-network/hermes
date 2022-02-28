fe7fb357cf13be7b5ecd30242072ecc70bad936f
davirian <davirain.yin@gmail.com>
2022-02-28 16:43:02 +0800

    add proven packet
    - TimeoutOnClose packet
    - TimeoutUnordered packet
    - TimeoutOrdered packet

--- relayer-cli/src/commands/start.rs
+++ relayer-cli/src/commands/start.rs
@@ -144,7 +144,6 @@ fn spawn_rest_server(config: &Arc<RwLock<Config>>) -> Option<rest::Receiver> {
 }
 
 #[cfg(feature = "telemetry")]
-
 fn spawn_telemetry_server(
     config: &Arc<RwLock<Config>>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
