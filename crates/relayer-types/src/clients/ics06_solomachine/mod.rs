8304a6cd8ebeb69faf66eff3ab16ac4675d75dd2
Yuanchao Sun <yuanchao.sun@gmail.com>
2023-03-20 23:25:44 +0800

    Fix failed to unmarshal proof into type *signing.SignatureDescriptor_Data: proto

--- crates/relayer-types/src/clients/ics06_solomachine/mod.rs
+++ crates/relayer-types/src/clients/ics06_solomachine/mod.rs
@@ -2,6 +2,7 @@ pub mod client_state;
 pub mod consensus_state;
 pub mod error;
 pub mod header;
+pub mod signing;
 
 pub const SOLOMACHINE_CLIENT_STATE_TYPE_URL: &str = "/ibc.lightclients.solomachine.v2.ClientState";
 pub const SOLOMACHINE_CONSENSUS_STATE_TYPE_URL: &str =
