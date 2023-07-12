fa85bfce2fb54f96c939a9727c50da68275dc615
Davirain <davirain.yin@gmail.com>
2023-07-12 17:37:51 +0800

    add tendermint-vp-sdk

---
+++ crates/tendermint-vp-sdk/src/channel_msg.rs
@@ -0,0 +1,52 @@
+use crate::types::Proofs;
+use crate::update_ic::{send_msg, send_msg_for_proofs};
+
+pub async fn channel_open_init(
+    canister_id: &str,
+    is_mainnet: bool,
+    msg: Vec<u8>,
+) -> Result<(), String> {
+    let method_name = "chan_open_init";
+    let args = msg;
+    send_msg(canister_id, method_name, args, is_mainnet).await
+}
+
+pub async fn channel_open_try(
+    canister_id: &str,
+    is_mainnet: bool,
+    msg: Vec<u8>,
+) -> Result<Proofs, String> {
+    let method_name = "chan_open_try";
+    let args = msg;
+    send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
+}
+
+pub async fn channel_open_ack(
+    canister_id: &str,
+    is_mainnet: bool,
+    msg: Vec<u8>,
+) -> Result<Proofs, String> {
+    let method_name = "chan_open_ack";
+    let args = msg;
+    send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
+}
+
+pub async fn channel_open_confirm(
+    canister_id: &str,
+    is_mainnet: bool,
+    msg: Vec<u8>,
+) -> Result<Proofs, String> {
+    let method_name = "chan_open_confirm";
+    let args = msg;
+    send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
+}
+
+pub async fn channel_close_init(
+    canister_id: &str,
+    is_mainnet: bool,
+    msg: Vec<u8>,
+) -> Result<(), String> {
+    let method_name = "chan_close_init";
+    let args = msg;
+    send_msg(canister_id, method_name, args, is_mainnet).await
+}
---
+++ crates/tendermint-vp-sdk/src/start_msg.rs
@@ -0,0 +1,44 @@
+use crate::update_ic::send_msg;
+
+pub async fn start_vp(canister_id: &str, is_mainnet: bool) -> Result<(), String> {
+    let method_name = "start";
+    let args = vec![];
+    send_msg(canister_id, method_name, args, is_mainnet).await
+}
+
+pub async fn restart_vp(canister_id: &str, is_mainnet: bool) -> Result<(), String> {
+    let method_name = "restart";
+    let args = vec![];
+    send_msg(canister_id, method_name, args, is_mainnet).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{restart_vp, start_vp};
+    use std::fs;
+    const PATH: &str = "tests/resource/canister_id";
+
+    #[test]
+    fn start_works() {
+        let canister_id = fs::read_to_string(PATH).expect("Read file error");
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            let ret = start_vp(&canister_id, false).await;
+            assert!(ret.is_ok());
+        });
+    }
+
+    #[test]
+    fn restart_works() {
+        let canister_id = fs::read_to_string(PATH).expect("Read file error");
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            let ret = restart_vp(&canister_id, false).await;
+            match ret {
+                Ok(_) => assert!(false),
+                Err(e) => {
+                    println!("error: {:?}", e.to_string());
+                    assert!(e.contains("unauthorized"));
+                }
+            }
+        });
+    }
+}
