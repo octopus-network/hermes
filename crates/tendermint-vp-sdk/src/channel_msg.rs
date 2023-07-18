87986508b0d5566fcf122eb4729847ac4c785cc9
Davirain <davirain.yin@gmail.com>
2023-07-18 16:39:52 +0800

    Update tendermint-vp-sdk dependencies and refactor function signatures (#123)
    or
    
    Refactor tendermint-vp-sdk dependencies and function signatures (#123)

--- crates/tendermint-vp-sdk/src/channel_msg.rs
+++ crates/tendermint-vp-sdk/src/channel_msg.rs
@@ -1,31 +1,20 @@
 use crate::types::Proofs;
 use crate::update_ic::{send_msg, send_msg_for_proofs};
+use anyhow::Result;
 
-pub async fn channel_open_init(
-    canister_id: &str,
-    is_mainnet: bool,
-    msg: Vec<u8>,
-) -> Result<(), String> {
+pub async fn channel_open_init(canister_id: &str, is_mainnet: bool, msg: Vec<u8>) -> Result<()> {
     let method_name = "chan_open_init";
     let args = msg;
     send_msg(canister_id, method_name, args, is_mainnet).await
 }
 
-pub async fn channel_open_try(
-    canister_id: &str,
-    is_mainnet: bool,
-    msg: Vec<u8>,
-) -> Result<Proofs, String> {
+pub async fn channel_open_try(canister_id: &str, is_mainnet: bool, msg: Vec<u8>) -> Result<Proofs> {
     let method_name = "chan_open_try";
     let args = msg;
     send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
 }
 
-pub async fn channel_open_ack(
-    canister_id: &str,
-    is_mainnet: bool,
-    msg: Vec<u8>,
-) -> Result<Proofs, String> {
+pub async fn channel_open_ack(canister_id: &str, is_mainnet: bool, msg: Vec<u8>) -> Result<Proofs> {
     let method_name = "chan_open_ack";
     let args = msg;
     send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
@@ -35,17 +24,13 @@ pub async fn channel_open_confirm(
     canister_id: &str,
     is_mainnet: bool,
     msg: Vec<u8>,
-) -> Result<Proofs, String> {
+) -> Result<Proofs> {
     let method_name = "chan_open_confirm";
     let args = msg;
     send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
 }
 
-pub async fn channel_close_init(
-    canister_id: &str,
-    is_mainnet: bool,
-    msg: Vec<u8>,
-) -> Result<(), String> {
+pub async fn channel_close_init(canister_id: &str, is_mainnet: bool, msg: Vec<u8>) -> Result<()> {
     let method_name = "chan_close_init";
     let args = msg;
     send_msg(canister_id, method_name, args, is_mainnet).await
--- crates/tendermint-vp-sdk/src/connection_msg.rs
+++ crates/tendermint-vp-sdk/src/connection_msg.rs
@@ -1,11 +1,8 @@
 use crate::types::Proofs;
 use crate::update_ic::{send_msg, send_msg_for_proofs};
+use anyhow::Result;
 
-pub async fn connection_open_init(
-    canister_id: &str,
-    is_mainnet: bool,
-    msg: Vec<u8>,
-) -> Result<(), String> {
+pub async fn connection_open_init(canister_id: &str, is_mainnet: bool, msg: Vec<u8>) -> Result<()> {
     let method_name = "conn_open_init";
     let args = msg;
     send_msg(canister_id, method_name, args, is_mainnet).await
@@ -15,7 +12,7 @@ pub async fn connection_open_try(
     canister_id: &str,
     is_mainnet: bool,
     msg: Vec<u8>,
-) -> Result<Proofs, String> {
+) -> Result<Proofs> {
     let method_name = "conn_open_try";
     let args = msg;
     send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
@@ -25,7 +22,7 @@ pub async fn connection_open_ack(
     canister_id: &str,
     is_mainnet: bool,
     msg: Vec<u8>,
-) -> Result<Proofs, String> {
+) -> Result<Proofs> {
     let method_name = "conn_open_ack";
     let args = msg;
     send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
@@ -35,7 +32,7 @@ pub async fn connection_open_confirm(
     canister_id: &str,
     is_mainnet: bool,
     msg: Vec<u8>,
-) -> Result<Proofs, String> {
+) -> Result<Proofs> {
     let method_name = "conn_open_confirm";
     let args = msg;
     send_msg_for_proofs(canister_id, method_name, args, is_mainnet).await
